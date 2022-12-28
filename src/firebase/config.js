d20fa5747478556ae859ce96e94783a3f2450293
Adilson Ortolan <65978702+adortolan@users.noreply.github.com>
2022-12-28 18:12:28 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore} from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDqH060hfHKRPPmadVf66avyHv15uWG9y4",
-  authDomain: "miniblog-261e2.firebaseapp.com",
-  projectId: "miniblog-261e2",
-  storageBucket: "miniblog-261e2.appspot.com",
-  messagingSenderId: "524332039657",
-  appId: "1:524332039657:web:a11c27be9cecca1c621f78"
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
